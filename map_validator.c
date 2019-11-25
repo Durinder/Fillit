6a99536c8f5bc6cedd055d8042cca01218256a14
Bianca Behm <bbehm@c2r1p5.hive.fi>
2019-11-25 17:19:31 +0200

    validator part that checks that the pieces are correct tetrominos done

--- map_validator.c
+++ map_validator.c
@@ -6,48 +6,76 @@
 /*   By: bbehm <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/25 11:07:10 by bbehm             #+#    #+#             */
-/*   Updated: 2019/11/25 12:04:28 by bbehm            ###   ########.fr       */
+/*   Updated: 2019/11/25 17:15:01 by bbehm            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_check_neighbours(char *map, int x, int y)
+{
+	if (x < 14 && x > 4)
+	{
+		if (map[x + 1] != '#' && map[x - 1] != '#' && map[x + 5] != '#'
+				&& map[x - 5] != '#')
+			return (0);
+	}
+	else if (x > 14)
+	{
+		if (map[x + 1] != '#' && map[x - 1] != '#' && map[x - 5] != '#')
+			return (0);
+	}
+	else if (x < 5 && x > 0)
+	{
+		if (map[x + 1] != '#' && map[x - 1] != '#' && map[x + 5] != '#')
+			return (0);
+	}
+	else if (x == 0)
+	{
+		if (map[x + 1] != '#' && map[x + 5] != '#')
+			return (0);
+	}
+	return (1);
+}
+
+int	ft_characters(char **map, int x, int y, int amount)
+{
+	int count;
+
+	count = 0;
+	while (map && y < amount)
+	{
+		while (map[y][x] != '\0' && x < 19)
+		{
+			if (map[y][x] == '#')
+			{
+				count++;
+				if (!(ft_check_neighbours(map[y], x, y)))
+					return (0);
+			}
+			x++;
+		}
+		if (count != 4)
+			return (0);
+		count = 0;
+		x = 0;
+		y++;
+	}
+	return (1);
+}
 
 int	ft_map_validator(char **map)
 {
 	int y;
 	int x;
-	int count;
+	int amount;
 
 	x = 0;
 	y = 0;
-	count = 0;
-	while (map)
-	{
-		while (map[y])
-		{
-			if (map[y][x] != '.' && map[y][x] != '#' && map[y][x] != '\n')
-				return (-1);
-			if (map[y][x] == '#' && x < 14 && x > 4)
-			{
-				count++;
-				if ((x < 14 && x > 4) && (map[y][x + 1] != '#' &&
-							map[y][x - 1] != '#' && map[y][x + 5] != '#' &&
-							map[y][x - 5] != '#'))
-					return (-1);
-				else if ((x > 14) && (map[y][x + 1] != '#' && map[y][x - 1]
-							!= '#' && map[y][x - 5] != '#'))
-					return (-1);
-				else if ((x < 5) && (map[y][x + 1] != '#' && map[y][x - 1]
-							!= '#' && map[y][x + 5] != '#'))
-					return (-1);
-			}
-			x++;
-		}
-		if (count != 4)
-			return (-1);
-		count = 0;
-		x = 0;
-		y++;
-	}
+	amount = 2;
+	if (!(ft_characters(map, x, y, amount)))
+		return (0);
 	if (y > 25)
-		return (-1);
-	return (0);
+		return (0);
+	return (1);
 }
