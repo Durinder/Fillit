91aec764c464fc4d9356501ad0cbe5410fdf09ff
Bianca Behm <bbehm@c2r1p5.hive.fi>
2019-11-27 12:55:47 +0200

    started making the struct structure in header

--- srcs/fillit.h
+++ srcs/fillit.h
@@ -6,7 +6,7 @@
 /*   By: jhallama <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/25 13:37:40 by jhallama          #+#    #+#             */
-/*   Updated: 2019/11/27 12:02:03 by jhallama         ###   ########.fr       */
+/*   Updated: 2019/11/27 12:55:25 by bbehm            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,15 @@
 # define FILLIT_H
 # define BUFF_SIZE 1024
 # include "../libft/libft.h"
+
+typedef struct		s_struct
+{
+	int		one[2];
+	int		two[2];
+	int		three[2];
+	int		four[2];
+	void	*next;
+}					t_struct;
 
 char	**create_tetriminos(char *input);
 void	tetriminos_into_alphabet(char **tetriminos);
