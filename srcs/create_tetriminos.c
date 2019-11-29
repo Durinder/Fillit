bf97c1f6d07be7597a75b7188f45172fc47efe1b
Joel Hallama <jhallama@student.hive.fi>
2019-11-29 15:10:53 +0200

    modified create_tetriminos.c to work for any amount of tetriminos (including one)

--- srcs/create_tetriminos.c
+++ srcs/create_tetriminos.c
@@ -6,28 +6,38 @@
 /*   By: jhallama <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/25 12:35:23 by jhallama          #+#    #+#             */
-/*   Updated: 2019/11/28 16:14:41 by jhallama         ###   ########.fr       */
+/*   Updated: 2019/11/29 15:09:29 by jhallama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft/libft.h"
 #include "fillit.h"
 
-void		copy_strings(char **array, char *input)
+void		null_terminator(char **array, short counter, short i)
+{
+	if (counter == 4)
+	{
+		array[0][21] = '\0';
+		array[1] = ft_strnew(0);
+		ft_strdel(&array[1]);
+	}
+	else
+		ft_strdel(&array[i]);
+}
+
+void		copy_strings(char **array, char *input, short counter)
 {
 	short	i;
 	short	j;
-	short	counter;
 
 	i = 0;
 	j = 0;
-	counter = 0;
 	array[i] = ft_strnew(21);
 	while (*input)
 	{
 		if (*input == '\n')
 			counter++;
-		if (counter != 0 && counter % 5 == 0)
+		if ((counter != 0 && counter % 5 == 0))
 		{
 			input++;
 			counter++;
@@ -37,7 +47,7 @@ void		copy_strings(char **array, char *input)
 		}
 		array[i][j++] = *input++;
 	}
-	ft_strdel(&array[i]);
+	null_terminator(array, counter, i);
 }
 
 short		count_tetriminos(short len)
@@ -50,31 +60,21 @@ short		count_tetriminos(short len)
 		return (len / 4 / 5);
 }
 
-t_checklist	modify_checklist(char c, t_checklist check)
-{
-	if (c == '.')
-		check.dot++;
-	else if (c == '#')
-		check.hash++;
-	else if (c == '\n')
-		check.nl++;
-	return (check);
-}
-
-short		count_length_check_errors(char *input)
+short		count_length_check_errors(char *input, t_checklist check)
 {
 	short		i;
-	t_checklist	check;
 
 	i = -1;
-	check.dot = 0;
-	check.hash = 0;
-	check.nl = 0;
 	while (input[++i])
 	{
+		if (input[i] == '.')
+			check.dot++;
+		else if (input[i] == '#')
+			check.hash++;
+		else if (input[i] == '\n')
+			check.nl++;
 		if (input[i] != '#' && input[i] != '.' && input[i] != '\n')
 			return (0);
-		check = modify_checklist(input[i], check);
 		if (check.nl == 4 || input[i + 1] == '\0')
 		{
 			if (check.dot != 12 || check.hash != 4 || (input[i + 1]
@@ -90,16 +90,21 @@ short		count_length_check_errors(char *input)
 
 char		**create_tetriminos(char *input)
 {
-	char	**array;
-	short	len;
-	short	amount;
+	char		**array;
+	short		len;
+	short		amount;
+	short		counter;
+	t_checklist	check;
 
-	if (!(len = count_length_check_errors(input)))
+	check.dot = 0;
+	check.hash = 0;
+	check.nl = 0;
+	if (!(len = count_length_check_errors(input, check)))
 		return (NULL);
 	if (!(amount = count_tetriminos(len)))
 		return (NULL);
 	if (!(array = (char **)malloc(sizeof(char *) * amount + 1)))
 		return (NULL);
-	copy_strings(array, input);
+	copy_strings(array, input, counter = 0);
 	return (array);
 }
