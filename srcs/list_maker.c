de9898bf3bc77773da406b4cfe9ec2bd74bb1cd6
Bianca Behm <bbehm@c2r4p6.hive.fi>
2019-11-29 16:52:02 +0200

    worked a bit further on list_maker and deleted the old file struct_maker

--- srcs/list_maker.c
+++ srcs/list_maker.c
@@ -6,14 +6,14 @@
 /*   By: jhallama <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/28 16:31:54 by jhallama          #+#    #+#             */
-/*   Updated: 2019/11/29 15:18:15 by jhallama         ###   ########.fr       */
+/*   Updated: 2019/11/29 16:49:42 by bbehm            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft/libft.h"
 #include "fillit.h"
 
-t_tetrimino	*create_piece()
+static t_tetrimino	*create_piece(int i)
 {
 	t_tetrimino	*piece;
 	
@@ -28,33 +28,63 @@ t_tetrimino	*create_piece()
 	piece->three[1] = 0;
 	piece->four[0] = 0;
 	piece->four[1] = 0;
-	piece->alpha = '#';
+	piece->alpha = 'A' + i;
 	piece->next = NULL;
 	return (piece);
 }
 
-void	push_back_piece(t_tetrimino *head)
+static void	push_back_piece(t_tetrimino *head, int i)
 {
 	t_tetrimino	*new_piece;
 	t_tetrimino	*current;
 
-	new_piece = create_piece();
+	new_piece = create_piece(i);
 	current = head;
 	while (current->next != NULL)
 		current = current->next;
 	current->next = new_piece;
+}/*
+static void fill_more(t_tetrimino *head, int *info)
+{
+
 }
 
+static void	fill_piece(t_tetrimino *head, char *tetri, int i)
+{
+	int x;
+	int *info[4][2];
+	int a;
+
+	x = 0;
+	a = 0;	
+	while (tetri[x] != '\0')
+	{
+		while (tetri[x] <= 'Z' || tetri[x] >= 'A')
+		{
+			if (tetri[x] == '\n')
+				y++;
+			x++;
+		}
+		if (tetri[x] == 'A' + i)
+		{
+			info[a][0] = y;
+			info[a++][1] = i;
+			i++;
+		}
+		fill_more(t_tetrimino *head, info);
+}
+*/
 t_tetrimino *tetriminos_into_list(char **tetriminos)
 {
 	t_tetrimino	*head;
 	short		i;
 
-	head = create_piece();
 	i = 0;
+	head = create_piece(i);
 	while (tetriminos[i])
 	{
-		push_back_piece(head);
+		push_back_piece(head, i + 1);
+		//fill_piece(head, tetriminos[i], i);
 		i++;
 	}
 	ft_strdel(tetriminos);
