408e28d70bb8e1dfdb6d3eb3d28cfe39ffe8fc8b
Bianca Behm <bbehm@c2r4p6.hive.fi>
2019-12-02 14:31:39 +0200

    fill piece works for all except first piece

--- srcs/list_maker.c
+++ srcs/list_maker.c
@@ -6,14 +6,14 @@
 /*   By: jhallama <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/28 16:31:54 by jhallama          #+#    #+#             */
-/*   Updated: 2019/11/29 17:20:35 by bbehm            ###   ########.fr       */
+/*   Updated: 2019/12/02 14:30:59 by bbehm            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft/libft.h"
 #include "fillit.h"
 
-static t_tetrimino	*create_piece(int i)
+static t_tetrimino	*create_piece(short i)
 {
 	t_tetrimino	*piece;
 	
@@ -33,7 +33,7 @@ static t_tetrimino	*create_piece(int i)
 	return (piece);
 }
 
-static void	push_back_piece(t_tetrimino *head, int i)
+static void	push_back_piece(t_tetrimino *head, short i)
 {
 	t_tetrimino	*new_piece;
 	t_tetrimino	*current;
@@ -45,24 +45,33 @@ static void	push_back_piece(t_tetrimino *head, int i)
 	current->next = new_piece;
 }
 
-static void fill_more(t_tetrimino *head, int info[4][2], int i)
+static void fill_more(t_tetrimino *head, int info[4][2], short i)
 {
 	while (i > 0)
 	{
 		head = head->next;
 		i--;
 	}
-	head->one[0] = info[0][0];
-	head->one[1] = info[0][1];
-	head->two[0] = info[1][0];
-	head->two[1] = info[1][1];
-	head->three[0] = info[2][0];
-	head->three[1] = info[2][1];
-	head->four[0] = info[3][0];
-	head->four[1] = info[3][1];
+	head->one[0] = (info[0][0] - info[0][0]);
+	head->one[1] = (info[0][1] - info[0][1]);
+	head->two[0] = (info[1][0] - info[0][0]);
+	head->two[1] = (info[1][1] - info[0][1]);
+	head->three[0] = (info[2][0] - info[0][0]);
+	head->three[1] = (info[2][1] - info[0][1]);
+	head->four[0] = (info[3][0] - info[0][0]);
+	head->four[1] = (info[3][1] - info[0][1]);
+	/*ft_putnbr(head->one[0]);
+	ft_putnbr(head->one[1]);
+	ft_putnbr(head->two[0]);
+	ft_putnbr(head->two[1]);
+	ft_putnbr(head->three[0]);
+	ft_putnbr(head->three[1]);
+	ft_putnbr(head->four[0]);
+	ft_putnbr(head->four[1]);*/
 }
 
-static void	fill_piece(t_tetrimino **head, char *tetri, int i)
+#include <stdio.h>
+static void fill_piece(t_tetrimino *head, char *tetri, short i)
 {
 	int x;
 	int info[4][2];
@@ -71,26 +80,24 @@ static void	fill_piece(t_tetrimino **head, char *tetri, int i)
 
 	x = 0;
 	a = 0;
-	y = 0;	
-	while (tetri[x] != '\0')
+	y = 0;
+	while (x < 19)
 	{
-		while (tetri[x] && (tetri[x] <= 'Z' || tetri[x] >= 'A'))
+		while (x < 19 && (tetri[x] >= 'Z' || tetri[x] <= 'A'))
 		{
 			if (tetri[x] == '\n')
 				y++;
 			x++;
 		}
-		if (tetri[x] == ('A' + i))
+		if (x < 19 && tetri[x] >= 'A' && tetri[x] <= 'Z')
 		{
 			info[a][0] = y;
-			info[a][1] = i;
-			i++;
+			info[a][1] = x % 5;
 			a++;
 			x++;
 		}
-		fill_more(*head, info, i);
 	}
-	(void)head;
+	fill_more(head, info, i);
 }
 
 t_tetrimino *tetriminos_into_list(char **tetriminos)
@@ -100,10 +107,11 @@ t_tetrimino *tetriminos_into_list(char **tetriminos)
 
 	i = 0;
 	head = create_piece(i);
+	fill_piece(head, tetriminos[i], i);
 	while (tetriminos[i])
 	{
 		push_back_piece(head, i + 1);
-		fill_piece(&head, tetriminos[i], i);
+		fill_piece(head, tetriminos[i], (i + 1));
 		i++;
 	}
 	ft_strdel(tetriminos);
